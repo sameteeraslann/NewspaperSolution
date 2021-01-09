f2926d657bbcd0a389bb17568d5661c0b28b9615
Samet <samettt18@hotmail.com.tr>
2021-01-09 21:47:16 +0300

    NewspaperSolution.Utility Kaymanı Eklendi.

---
+++ NewspaperSolution.UI/Areas/Admin/Controllers/AppUserController.cs
@@ -0,0 +1,45 @@
+﻿using NewspaperSolution.DataAccessLayer.Repositories.Concrete.EfRepositories;
+using NewspaperSolution.EntityLayer.Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewspaperSolution.UI.Areas.Admin.Controllers
+{
+    public class AppUserController : Controller
+    {
+        private EfAppUserRepository _repo;
+        // GET: Admin/AppUser
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(AppUser data, HttpPostedFile Image)
+        {
+            List<string> UploadImagePaths = new List<string>();
+            UploadImagePaths = ImageUploader.UploadSingleImage(ImageUploader.OriginalProfileImagePath, Image, 1);
+
+            data.UserImage = UploadImagePaths[0];
+
+            if (data.UserImage == "1" || data.UserImage == "2" || data.UserImage == "3")
+            {
+                data.UserImage = ImageUploader.DefaultProfileImagePath;
+                data.XSmallUserImage = ImageUploader.DefaultXSmallProfileImagePath;
+                data.CruptedUserImage = ImageUploader.DefaultCruptedProfileImagePath;
+            }
+            else
+            {
+                data.XSmallUserImage = UploadImagePaths[1];
+                data.CruptedUserImage = UploadImagePaths[2];
+            }
+
+            _repo.Add(data);
+            return Redirect("/Admin/AppUser/List");
+        }
+        
+    }
+}
