b1e9041e37b3c30c3cf4ad91a946b767bfbe93d3
Samet <samettt18@hotmail.com.tr>
2021-01-10 15:52:37 +0300

    Admin Home Page Eklendi

---
+++ NewspaperSolution.UI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using NewspaperSolution.DataAccessLayer.Repositories.Concrete.EfRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewspaperSolution.UI.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        EfPostRepository _postRepository;
+        public HomeController() => _postRepository = new EfPostRepository();
+       
+        // GET: Admin/Home
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
